75257e9693943228e5e98e752b0b8edb6ace3ec3
miguel <miganrobles@gmail.com>
2016-02-15 13:24:47 +0100

    Codificado método printProductDetails que imprime por pantalla los detalles de todos los productos que hay en nuestro stock

--- StockManager.java
+++ StockManager.java
@@ -66,5 +66,8 @@ public class StockManager
      */
     public void printProductDetails()
     {
+        for (Product producto : stock) {
+            System.out.println(producto.toString());
+        }
     }
 }
